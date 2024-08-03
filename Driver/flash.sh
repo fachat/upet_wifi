e60114c545579c8bb6512bd550b5c2e70d0cd1a0
Andre Fachat <afachat@gmx.de>
2024-08-03 12:17:27 +0200

    add sendbrk

---
+++ Driver/flash.sh
@@ -0,0 +1,5 @@
+
+
+
+~/.local/bin/esptool.py --chip auto --port PORTNAME --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size 4MB 0x0 FILEDIRECTORY
+
